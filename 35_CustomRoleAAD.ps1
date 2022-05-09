7a2f14d3c470502863fb8074dcb1cb1daa2009d9
www42 <thomas.jaekel@gfn.training>
2022-05-09 20:33:30 +0200

    Not ready yet

--- 35_CustomRoleAAD.ps1
+++ 35_CustomRoleAAD.ps1
@@ -1,25 +1,46 @@
-
-# Want to have a list of Azure AD roles, but only 3 roles appear
-Get-AzureADDirectoryRole
-
-# This is expected. Only roles with an active assignment appear in the list.
-# If you want to use other roles (without active assignement) you have to enable this role
-Get-AzureADDirectoryRoleTemplate | Sort-Object DisplayName | Format-Table DisplayName, Description
-$Role = Get-AzureADDirectoryRoleTemplate | Where-Object DisplayName -EQ "Application Administrator" 
-Enable-AzureADDirectoryRole -RoleTemplateId $Role.ObjectId
-
-Get-AzureADDirectoryRole
-$AppAdminRole = Get-AzureADDirectoryRole | Where-Object DisplayName -eq 'Application Administrator'
-
-Get-AzureADUser -All:$true
-$Admin = Get-AzureADUser -All:$true | Where-Object DisplayName -EQ "MOD Administrator"
-
-Get-AzureADUserOAuth2PermissionGrant -ObjectId $Admin.ObjectId
-Get-AzureADUserOAuth2PermissionGrant -ObjectId $Admin.ObjectId | Where-Object ResourceId -EQ a4d00705-a19e-425e-b48f-629992629850 | Format-List *
-
+# This requires module 'AzureAD' (running on Windows PowerShell only).
+# Module 'AzureAD.Standard.Preview' lacks the AzureADMS* cmdlets :-(
+# Test:
 Get-Command -Name Get-AzureADMSRoleDefinition
-Get-AzureADMSRoleDefinition -ObjectId $AppAdminRole.ObjectId
 
-$AppAdminRole           = Get-AzureADDirectoryRoleTemplate | Where-Object DisplayName -EQ "Application Administrator"
-$AppAdminRoleDefinition = Get-AzureADMSRoleDefinition -Id $AppAdminRole.ObjectId
-$AppAdminRoleDefinition | % RolePermissions | % AllowedResourceActions
+# Hint: Do not use 
+#   Get-AzureADDirectoryRole
+#   Get-AzureADDirectoryRoleTemplate
+
+
+# -----------------------
+# Azure AD built in roles
+# -----------------------
+Get-AzureADMSRoleDefinition | Sort-Object DisplayName | Format-Table DisplayName,Description
+$Role = Get-AzureADMSRoleDefinition -Filter "displayName eq 'Application Administrator'"
+$Role.RolePermissions.AllowedResourceActions
+
+$GlobalAdministrator = Get-AzureADMSRoleDefinition -Filter "displayName eq 'Global Administrator'"
+
+$User = Get-AzureADUser -Filter "userPrincipalName eq 'Wolfgang.Pauli@trainymotion.com'"
+$Paul = Get-AzureADUser -Filter "userPrincipalName eq 'Paul@trainymotion.com'"
+
+# OData Filter
+$GlobalAdministrator.Id
+                                Get-AzureADMSRoleAssignment -Filter "roleDefinitionId eq '62e90394-69f5-4237-9190-012177145e10'" # Works
+                                Get-AzureADMSRoleAssignment -Filter "roleDefinitionId eq '$GlobalAdministrator.Id'"              # Does not work
+$foo = $GlobalAdministrator.Id; Get-AzureADMSRoleAssignment -Filter "roleDefinitionId eq '$foo'"                                 # Works
+                                Get-AzureADMSRoleAssignment -Filter "roleDefinitionId eq '$($GlobalAdministrator.Id)'"           # Works - magic!
+
+New-AzureADMSRoleAssignment -RoleDefinitionId $Role.Id -PrincipalId $User.ObjectId -DirectoryScopeId '/'
+
+Get-AzureADMSRoleAssignment -Filter "principalId eq '$($User.ObjectId)'"
+Get-AzureADMSRoleAssignment -Filter "roleDefinitionId eq '$($Role.Id)'"
+$foo = $Role.Id
+Get-AzureADMSRoleAssignment -Filter "roleDefinitionId eq '$foo'"
+Get-AzureADGroupMember -ObjectId (Get-AzureADGroup -Filter "DisplayName eq '$GroupName'").ObjectId | ft DisplayName,UserType
+
+
+
+
+# ------------------------
+# Azure AD custom in roles
+# ------------------------
+
+# Reminder: Azure AD custom roles require premium license
+Get-AzureADMSRoleDefinition -Filter "displayName eq 'TM Foo Administrator'"
