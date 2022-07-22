042223d60dee310cdb2fa5d1224138fdfbfcc201
www42 <thomas.jaekel@gfn.training>
2022-07-22 21:00:53 +0200

    Minor changes

--- scripts/script42.ps1
+++ scripts/script42.ps1
@@ -1,3 +1,9 @@
+# Install NuGet in order to install modules
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+# Install more DSC resources e.g. 'TimeZone'
+Install-Module -Name ComputerManagementDsc -Force
+
 # Create temp folder
 # (Azure VM custom script extension cannot use user profile as destination, C:\Users\Student does not exist at that time)
 New-Item -ItemType Directory -Path C:\temp -Force
