53e34a5fb43a84ae8c96add8223c151c94eee7b9
www42 <thomas.jaekel@gfn.training>
2022-07-24 15:18:54 +0200

    New script1.ps1

--- scripts/script1.ps1
+++ scripts/script1.ps1
@@ -8,23 +8,9 @@ Install-Module -Name ComputerManagementDsc -Force
 # (Azure VM custom script extension cannot use user profile as destination, C:\Users\Student does not exist at that time)
 New-Item -ItemType Directory -Path C:\temp -Force
 
-# Download files for demo "Managed Identity"
-$source = 'https://raw.githubusercontent.com/www42/AZ-303-Microsoft-Azure-Architect-Technologies/master/tj/demo-Mi/demo-Mi-Token.ps1'
-$destination =  'C:\temp\demo-Mi-Token.ps1'
-Invoke-WebRequest -Uri $source -OutFile $destination
-
-$source = 'https://raw.githubusercontent.com/www42/AZ-303-Microsoft-Azure-Architect-Technologies/master/tj/demo-Mi/demo-Mi-AzContext.ps1'
-$destination =  'C:\temp\demo-Mi-AzContext.ps1'
-Invoke-WebRequest -Uri $source -OutFile $destination
-
 # Download zoomit ver 4.50 (works with pen)
 $source = 'https://github.com/www42/TrainyMotion/raw/master/tools/ZoomIt.exe'
 $destination = 'C:\temp\ZoomIt.exe'
-Invoke-WebRequest -Uri $source -OutFile $destination
-
-# Download CpuStress
-$source = 'https://download.sysinternals.com/files/CPUSTRES.zip'
-$destination = 'C:\temp\CPUSTRES.zip'
 Invoke-WebRequest -Uri $source -OutFile $destination
 
 # Download WmiExplorer
