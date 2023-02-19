95c3f2072aa582e5e6c7795fc7316908edf69bf0
www42 <thomas.jaekel@gfn.training>
2023-02-19 16:50:12 +0100

    Change file extension to s1

--- 240_HybridInfrastructure.ps1
+++ 240_HybridInfrastructure.ps1
@@ -3,27 +3,36 @@
 # -------------------------------------------------------------------
 $rgName='OnPrem-RG'
 $location='westeurope'
-$file='Hybrid/hybrid.bicep'
+$templateFile='Hybrid/hybrid.bicep'
+dir $templateFile
 
-az group create --name $rgName --location $location
+# az group create --name $rgName --location $location
+New-AzResourceGroup -Name $rgName -Location $location
 
 # read -sp "Enter vmAdminPassword: " adminPassword
 
-
-az deployment group create --resource-group $rgName --template-file $file --parameters vmAdminUserName='localadmin' vmAdminPassword=''
-
+# az deployment group create --resource-group $rgName --template-file $templateFile --parameters vmAdminUserName='localadmin' vmAdminPassword=''
+$templateParams = @{
+    vmAdminUserName = 'localadmin'
+    vmAdminPassword = '6kZrBmyy1GjpA5'
+    vnetName = "OnPrem-VNet"
+}
+New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile $templateFile -TemplateParameterObject $templateParams
 
 
 
 # -------------------------------------------------------------------
 # Tests, useful commands, clean up
 # -------------------------------------------------------------------
-az account show
+# az account show
+Get-AzContext | fl *
 
-az group list --output table
+# az group list --output table
+Get-AzResourceGroup | ft ResourceGroupName,Location,ProvisioningState
 
 az deployment group list --resource-group $rgName \
     --query "reverse(sort_by([].{Name:name,provisioningState:properties.provisioningState,timestamp:properties.timestamp}, &timestamp))" --output table
+Get-AzResourceGroupDeployment -ResourceGroupName $rgName | Sort-Object Timestamp | ft DeploymentName,ProvisioningState
 
 az resource list --resource-group $rgName --query "sort_by([].{name:name,Type:type,location:location},&Type)" --output table
 
