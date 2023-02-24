7aea7667ca662c63654f63adf0774b7891432df8
www42 <tjaekel0815@gmail.com>
2023-02-24 17:17:25 +0100

    Delete aa bicep

--- 240_HybridIdentity_Infrastructure.ps1
+++ 240_HybridIdentity_Infrastructure.ps1
@@ -1,20 +1,21 @@
-# --------------------------------------------------------------------------------------------
-# Deployment virtual network, bastion host, automation account, domain controller-vm, and more
-# --------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------
+# Deployment virtual network, bastion host, automation account, and domain controller-vm
+# ---------------------------------------------------------------------------------------
 $templateFile='HybridIdentity/main.bicep'
 $templateParams = @{
     location = 'westeurope'
     resourceGroupName = 'OnPrem-RG'
     vnetName = 'OnPrem-VNet'
     automationAccountName = 'Hybrid-AutomationAccount'
-    createAaJob = $false
+    createAaJob = $true
     domainName = 'az.training'
     domainAdminName = 'DomainAdmin'
     domainAdminPassword = 'Pa55w.rd1234'
-    # localAdminName = 'LocalAdmin'
-    # localAdminPassword = 'Pa55w.rd1234'
 }
 New-AzSubscriptionDeployment -Name 'HybridIdentity' -TemplateFile $templateFile -TemplateParameterObject $templateParams -Location $templateParams.location
+
+
+$templateParams['createAaJob'] = $false    
 
 
 
@@ -27,8 +28,8 @@ Get-AzContext | fl *
 # az group list --output table
 Get-AzResourceGroup | ft ResourceGroupName,Location,ProvisioningState
 
-az deployment group list --resource-group $rgName \
-    --query "reverse(sort_by([].{Name:name,provisioningState:properties.provisioningState,timestamp:properties.timestamp}, &timestamp))" --output table
+# az deployment group list --resource-group $rgName \
+#     --query "reverse(sort_by([].{Name:name,provisioningState:properties.provisioningState,timestamp:properties.timestamp}, &timestamp))" --output table
 Get-AzResourceGroupDeployment -ResourceGroupName $rgName | Sort-Object Timestamp | ft DeploymentName,ProvisioningState
 
 az resource list --resource-group $rgName --query "sort_by([].{name:name,Type:type,location:location},&Type)" --output table
@@ -37,5 +38,5 @@ az resource list --resource-group $rgName --query "sort_by([].{name:name,Type:ty
 az deployment group create --name 'tabulaRasa' --resource-group $rgName --mode Complete --template-uri "https://raw.githubusercontent.com/www42/arm/master/templates/empty.json" --no-wait
 
 # Delete resource group
-az group delete --resource-group $rgName --yes --no-wait
+# az group delete --resource-group $rgName --yes --no-wait
 Remove-AzResourceGroup -Name $templateParams.resourceGroupName -Force
