f28c07685aa07a712d8170fc37a16bc3d9407215
www42 <tjaekel0815@gmail.com>
2023-05-06 14:51:34 +0200

    Create shortcut

--- 262_NestedVirtualization_NestedVMs.ps1
+++ 262_NestedVirtualization_NestedVMs.ps1
@@ -1,2 +1,6 @@
-# Download eval version Windows Server
-#   https://techcommunity.microsoft.com/t5/windows-11/accessing-trials-and-kits-for-windows-eval-center-workaround/m-p/3361125
+
+# Shortcut on Desktop to Eval Centerround
+$wsShell = New-Object -ComObject WScript.Shell
+$shortcut = $wsShell.CreateShortcut("$HOME\Desktop\EvalCenter.lnk")
+$shortcut.TargetPath = "https://techcommunity.microsoft.com/t5/windows-11/accessing-trials-and-kits-for-windows-eval-center-workaround/m-p/3361125"
+$shortcut.Save()
