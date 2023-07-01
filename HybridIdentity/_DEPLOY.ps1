14fa2aa92c4be7a156e30f810233e7b9e4e83c58
www42 <tjaekel0815@gmail.com>
2023-07-01 17:42:20 +0200

    Add template windowsClient

--- HybridIdentity/_DEPLOY.ps1
+++ HybridIdentity/_DEPLOY.ps1
@@ -13,11 +13,7 @@
 Login-AzAccount
 Get-AzContext | Format-List Name,Account,Tenant,Subscription
 
-
-
 # ------------------------------------------------------------------------------------
-# DEPLOYMENT
-#
 $templateFile = 'HybridIdentity/templates/main.bicep'
 $templateParams = @{
     location = 'westeurope'
@@ -29,43 +25,61 @@ $templateParams = @{
     domainAdminName = 'DomainAdmin'
     domainAdminPassword = ''
 }
-$templateParams['domainName'] = 'az.training'
+$templateParams['domainName'] = 'adatum.com'
 $templateParams['createAaJob'] = $false
 $templateParams['domainAdminPassword'] = ''
 
 New-AzSubscriptionDeployment -Name 'Hybrid-Identity-Scenario' -TemplateFile $templateFile -TemplateParameterObject $templateParams -Location $templateParams.location -ResourceGroupName $templateParams.resourceGroupName
-#
 # ------------------------------------------------------------------------------------
 
 
-
-
-
-
-# -------------------------------------------------------------------
-# Tests, useful commands, clean up
-# -------------------------------------------------------------------
-Get-AzContext | Format-List *
-Get-AzResourceGroup | ft ResourceGroupName,Location,ProvisioningState
-Get-AzSubscriptionDeployment
-Get-AzSubscriptionDeployment | Sort-Object Timestamp | ft DeploymentName, ProvisioningState, Timestamp
+# Resource group
 $rgName = $templateParams.resourceGroupName
-Get-AzResourceGroupDeployment -ResourceGroupName $rgName | Sort-Object Timestamp | ft DeploymentName,ProvisioningState,Timestamp
+Get-AzResourceGroup | Sort-Object ResourceGroupName | ft ResourceGroupName,Location,ProvisioningState
 Get-AzResource -ResourceGroupName $rgName | Sort-Object ResourceType | Format-Table Name,ResourceType,Location
+
+# Deployments
+Get-AzSubscriptionDeployment | Sort-Object Timestamp | ft DeploymentName, ProvisioningState, Timestamp
+Get-AzResourceGroupDeployment -ResourceGroupName $rgName | Sort-Object Timestamp | ft DeploymentName,ProvisioningState,Timestamp
 
 New-AzResourceGroupDeployment -Name 'tabulaRasa' -ResourceGroupName $rgName -Mode Complete -Force -TemplateUri 'https://raw.githubusercontent.com/www42/arm/master/templates/empty.json' -AsJob
 Remove-AzResourceGroup -Name $rgName -Force -AsJob
 
 
+# Virtual network https://learn.microsoft.com/en-us/powershell/module/az.network/?view=azps-9.4.0#virtual-network
+Get-AzVirtualNetwork | ft Name,AddressSpace,Subnets
+Get-AzVirtualNetwork | % { $_.Subnets } | ft Name,AddressPrefix
+# $vnetName = $templateParams.vnetName
+$vnetName = 'vnet-hybrid'
+Get-AzVirtualNetwork -ResourceGroupName $rgName -Name $vnetName | fl Name,Subnets
+Get-AzVirtualNetworkSubnetConfig -VirtualNetwork (Get-AzVirtualNetwork -ResourceGroupName $rgName -Name $vnetName) -Name 'default' | Tee-Object -Variable subnet| fl Name,AddressPrefix,NetworkSecurityGroup,RouteTable
+$subnet.Id
+
+New-AzResourceGroupDeployment `
+    -Name 'WindowsClient-Deployment' `
+    -ResourceGroupName $rgName `
+    -TemplateFile 'HybridIdentity/templates/windowsClient.bicep' `
+    -TemplateParameterObject @{
+        location = 'westeurope';
+        vmName = 'Client001';
+        vmAdminUserName = 'localadmin'
+        vmAdminPassword = ''
+        vmSubnetId = $subnet.Id
+    }
+
+
+
+
+
 # Automation account https://learn.microsoft.com/en-us/powershell/module/az.automation/?view=azps-9.4.0#automation
 $aaName = $templateParams.automationAccountName
-Get-AzAutomationAccount -ResourceGroupName $rgName -Name $aaName
-Get-AzAutomationRegistrationInfo -ResourceGroupName $rgName -AutomationAccountName $aaName
-Get-AzAutomationDscConfiguration -ResourceGroupName $rgName -AutomationAccountName $aaName
-Get-AzAutomationDscCompilationJob -ResourceGroupName $rgName -AutomationAccountName $aaName -OutVariable aaJob
+Get-AzAutomationAccount -ResourceGroupName $rgName -Name $aaName | fl AutomationAccountName,Plan,State
+Get-AzAutomationRegistrationInfo -ResourceGroupName $rgName -AutomationAccountName $aaName | fl AutomationAccountName,PrimaryKey,SecondaryKey,Endpoint
+Get-AzAutomationDscConfiguration -ResourceGroupName $rgName -AutomationAccountName $aaName | fl AutomationAccountName,Name,State
+Get-AzAutomationDscCompilationJob -ResourceGroupName $rgName -AutomationAccountName $aaName | Tee-Object -Variable aaJob | fl AutomationAccountName,ConfigurationName,Status
 Get-AzAutomationDscCompilationJobOutput -ResourceGroupName $rgName -AutomationAccountName $aaName -Id $aaJob.Id | Format-Table Time,Type,Summary
-Get-AzAutomationDscNodeConfiguration -ResourceGroupName $rgName -AutomationAccountName $aaName
-Get-AzAutomationDscNode -ResourceGroupName $rgName -AutomationAccountName $aaName
+Get-AzAutomationDscNodeConfiguration -ResourceGroupName $rgName -AutomationAccountName $aaName | fl AutomationAccountName,ConfigurationName,Name,RollupStatus
+Get-AzAutomationDscNode -ResourceGroupName $rgName -AutomationAccountName $aaName | fl AutomationAccountName,Name,NodeConfigurationName,LastSeen,Status
 
 
 # TODO
---
+++ HybridIdentity/templates/windowsClient.bicep
@@ -0,0 +1,78 @@
+param location string
+param vmName string = 'Client01'
+param vmSize string = 'Standard_D2s_v3'
+param vmAdminUserName string
+@secure()
+param vmAdminPassword string
+param vmSubnetId string
+
+
+var vmOsDiskName = '${vmName}-Disk'
+var vmComputerName = vmName
+var vmNicName = '${vmName}-Nic'
+var vmNsgName = '${vmName}-Nsg'
+
+
+
+
+resource vm 'Microsoft.Compute/virtualMachines@2023-03-01' = {
+  name: vmName
+  location: location
+  properties: {
+    hardwareProfile: {
+      vmSize: vmSize
+    }
+    storageProfile: {
+      imageReference: {
+        publisher: 'MicrosoftWindowsDesktop'
+        offer: 'windows-11'
+        sku: 'win11-22h2-ent'
+        version: 'latest'
+      }
+      osDisk: {
+        name: vmOsDiskName
+        caching: 'ReadWrite'
+        createOption: 'FromImage'
+      }      
+    }
+    osProfile: {
+      computerName: vmComputerName
+      adminUsername: vmAdminUserName
+      adminPassword: vmAdminPassword
+      windowsConfiguration: {
+        timeZone: 'W. Europe Standard Time'
+      }
+    }
+    networkProfile: {
+      networkInterfaces: [
+        {
+          id: vmNic.id
+        }
+      ]
+    }
+  }
+}
+resource vmNic 'Microsoft.Network/networkInterfaces@2022-11-01' = {
+  name: vmNicName
+  location: location
+  properties: {
+    networkSecurityGroup: {
+      id: vmNsg.id
+    }
+    ipConfigurations: [
+      {
+        name: 'ipConfig1'
+        properties: {
+          privateIPAllocationMethod: 'Dynamic'
+          subnet: {
+            id: vmSubnetId
+          }
+        }
+      }
+    ]
+  }
+}
+resource vmNsg 'Microsoft.Network/networkSecurityGroups@2022-11-01' = {
+  name: vmNsgName
+  location: location
+}
